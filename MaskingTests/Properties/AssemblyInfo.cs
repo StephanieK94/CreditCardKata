805d19b7ee310355e08f8ac8a3901224c0cf3254
StephanieK <stephanie.kitchen@serko.com>
2019-06-18 14:45:36 +1200

    Creation of Unit Test, first failure and creation of MaskingClasses in order to pass

---
+++ MaskingTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MaskingTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MaskingTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2c3fabd5-f49c-411e-a62b-d0f5a0482868")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MaskingTests/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MaskingClasses;
+
+namespace MaskingTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string input = "12345";
+
+            Masker ms = new Masker();
+
+            string actual = ms.MaskSecureNumbers(input);
+
+            Assert.AreEqual("", actual);
+        }
+    }
+}
